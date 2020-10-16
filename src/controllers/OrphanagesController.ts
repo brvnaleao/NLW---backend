de5dd2fa3bfa9b68b3e26cc5d67a6e89519b08f6
brvnaleao <lleaobruna@gmail.com>
2020-10-16 07:41:30 -0300

    backend finished

--- src/controllers/OrphanagesController.ts
+++ src/controllers/OrphanagesController.ts
@@ -2,21 +2,26 @@
 import { getRepository } from 'typeorm';
 import { Request, Response } from 'express'
 import Orphanage from '../model/Orphanage';
-
+import orphanageView from '../views/orphanages_view'
+import * as Yup from 'yup'
 export default {
     async index(req: Request, res: Response){
         const orphanagesRepository = getRepository(Orphanage);
-        const orphanages = await orphanagesRepository.find();
+        const orphanages = await orphanagesRepository.find({
+            relations: ['images']
+        });
 
-        return res.json(orphanages)
+        return res.json(orphanageView.renderMany(orphanages))
 
     },
     async show(req: Request, res: Response){
         const id = req.params
         const orphanagesRepository = getRepository(Orphanage);
-        const orphanages = await orphanagesRepository.findOneOrFail(id);
+        const orphanages = await orphanagesRepository.findOneOrFail(id, {
+            relations: ['images']
+        });
 
-        return res.json(orphanages)
+        return res.json(orphanageView.render(orphanages))
 
     },
 
@@ -32,8 +37,14 @@ export default {
         } = req.body;
     
         const orphanagesRepository = getRepository(Orphanage);
-    
-        const orphanage = orphanagesRepository.create({
+
+        const requestImages = req.files as Express.Multer.File[];
+        const images = requestImages.map( image =>{
+            return {
+                path: image.filename
+            }
+        })
+        const data = {
             name,
             latitude,
             longitude,
@@ -41,14 +52,28 @@ export default {
             instructions,
             opening_hours, 
             open_on_weekends,
+            images
+        }
+
+        const schema = Yup.object().shape({
+            name : Yup.string().required().max(100),
+            latitude: Yup.number().required(),
+            longitude: Yup.number().required(),
+            about: Yup.string().required().max(300),
+            instructions: Yup.string().required().max(500),
+            opening_hours: Yup.string().required().max(500),
+            open_on_weekends: Yup.string().required().max(500),
+            images: Yup.array(Yup.object().shape({
+                path: Yup.string().required()
+            }))
         })
-        
-        try{
-            await orphanagesRepository.save(orphanage);
-        }
-        catch(e){
-            return res.status(400).json({erro: "sim"})
-        }
+        await schema.validate(data, {
+            abortEarly: false
+        })
+        const orphanage = orphanagesRepository.create(data)
+    
+        await orphanagesRepository.save(orphanage);
+  
         return res.status(201).json(orphanage)
     }
 }
--- src/model/Orphanage.ts
+++ src/model/Orphanage.ts
@@ -1,32 +1,35 @@
-import { Entity, Column, PrimaryGeneratedColumn} from 'typeorm'
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany, JoinColumn } from 'typeorm';
+import Image from './Image';
 
 @Entity('orphanages')
 export default class Orphanage {
     @PrimaryGeneratedColumn('increment')
-   id: number;
+    id: number;
 
-   @Column()
-   name: string;
+    @Column()
+    name: string;
 
-   @Column()
+    @Column()
     latitude: number;
 
     @Column()
-   longitude: number;
-   
-   @Column()
-   about: string;
+    longitude: number;
 
-   @Column()
-   instructions: string;
-   
-   @Column()
-   opening_hours: string;
+    @Column()
+    about: string;
 
-   @Column()
-   open_on_weekends: boolean;
+    @Column()
+    instructions: string;
 
+    @Column()
+    opening_hours: string;
 
-
-
+    @Column()
+    open_on_weekends: boolean;
+    
+    @OneToMany(() => Image, image => image.orphanage, {
+        cascade: ['insert', 'update']
+    })
+    @JoinColumn({ name: 'orphanage_id'})
+    images: Image[];
 }
